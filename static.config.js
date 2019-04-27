34626200b5e958fa10599b402eb031fd0629f808
isett <admin@isett.de>
2019-04-27 17:05:17 +0200

    Google Analytics

--- static.config.js
+++ static.config.js
@@ -150,6 +150,15 @@ export default {
             {renderMeta.styleTags}
           </Head>
           <Body>{children}</Body>
+          <!-- Global site tag (gtag.js) - Google Analytics -->
+          <script async src="https://www.googletagmanager.com/gtag/js?id=UA-85012381-1"></script>
+          <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'UA-85012381-1');
+          </script>
         </Html>
       )
     }
